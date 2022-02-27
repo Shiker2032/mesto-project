c65c5b3767de5f4dbe5f131e60c64ab2a95c34f9
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-02-27 21:01:22 +0300

    Сделал основу валидации форм

--- script.js
+++ script.js
@@ -146,6 +146,87 @@ document.addEventListener('keydown', (evt) => {
 	}
 })
 
-
 //----------------------------------------- Валидация форм -------------------------------------------------------------
 
+const validationconfig = {
+	formSelector: '.form',
+	inputSelector: '.form__item',
+	buttonSelector: '.form__button',
+	buttonDisableClass: 'form__button_disabled',
+	errorClass: 'input__error_visible',
+	inputErrorClass : 'input__element_invalid'
+}
+
+ function hideInputError(inputElement, errorElement, config) {
+	inputElement.classList.remove(config.inputErrorClass);
+	errorElement.classList.remove(config.errorClass);
+	errorElement.textContent = '';
+ }
+
+ function showInputError(inputElement, errorElement, errorMessage, config) {
+	inputElement.classList.add(config.inputErrorClass);
+	errorElement.classList.add(config.errorClass);
+	errorElement.textContent = errorMessage;
+ }
+
+ function disableButton (buttonElement, config) {
+	 buttonElement.classList.add(config.buttonDisableClass);
+	 buttonElement.disabled = true;
+ }
+
+ function enableButton (buttonElement, config) {
+	buttonElement.classList.remove(config.buttonDisableClass);
+	buttonElement.disabled = false;
+}
+
+function hasInvalidInput (inputList) {
+	return inputList.some((inputElement) => {
+		return inputElement.validity.valid === false;
+	})
+}
+
+ function toggleButtonState (formElement, inputList, config) {
+	 const buttonElement = formElement.querySelector(config.buttonSelector);
+
+	 if (hasInvalidInput(inputList)) {
+		 disableButton(buttonElement, config);
+	 } else {
+		 enableButton(buttonElement, config)
+	 }
+ }
+
+function checkInputValidity (formElement, inputElement, config) {
+	const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
+	if (inputElement.validity.valid) {
+		hideInputError(inputElement, errorElement, config);
+	} else {
+		showInputError(inputElement, errorElement, inputElement.validationMessage, config);
+	}
+}
+
+function setEventListeners (formElement, config) {
+	const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+
+	inputList.forEach((inputElement) => {
+		inputElement.addEventListener('input', () => {
+			// Проверка валидации этого инпута
+			checkInputValidity(formElement, inputElement, config);
+			// Проверять состояние кнопки сабмита			
+			toggleButtonState(formElement, inputList, config);
+		});
+		toggleButtonState(formElement, inputList, config);
+	});
+}
+
+function enableValidation (config) {
+	const formElements = Array.from(document.querySelectorAll(validationconfig.formSelector));	
+
+	formElements.forEach((formElement) => {
+		formElement.addEventListener('submit', (evt) => {
+			evt.preventDefault();			
+		});
+		setEventListeners(formElement, config);
+	});
+};
+
+enableValidation(validationconfig);
