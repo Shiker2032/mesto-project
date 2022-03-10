cfc9289fe6b032caf22c6b70dd32e6f1e0947e75
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-03-10 19:01:43 +0300

    Форма редактирования аватара профиля

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,34 +6,50 @@ const popupCard = document.querySelector("[id='popup-card']");
 const cardCloseButton = document.querySelector("[id='card-form-close']");
 const popupImageContainer = document.querySelector("#popup-image-container");
 const imageContainerCloseBtn = document.querySelector("#popup-image__close-button");
+const popupAvatarEdit = document.querySelector('#popup-avatar-edit');
+const editAvatarBtn = document.querySelector(".profile__avatar");
+const closeAvatarForm = document.querySelector('#avatar-form-close')
+
+
+
+editAvatarBtn.addEventListener('click', () => togglePopup(popupAvatarEdit));
 
 function togglePopup(popup) {
-
-	popup.classList.toggle("popup_state_visible");  
-	popup.addEventListener("click", (evt) => {
-		if (evt.target.classList.contains("popup")) togglePopup(popup);
-	})
-	document.addEventListener('keydown',  closeByEsc)
+	if (popup.classList.contains('popup_state_visible')){
+		document.removeEventListener('keydown', closeByEsc);
+		popup.removeEventListener('click', closeByClick);
+	} else {
+		document.addEventListener('keydown', closeByEsc);
+		popup.addEventListener('click', closeByClick);
+	}
+	popup.classList.toggle("popup_state_visible"); 	
 }
 
 function findActivePopup () {
 	return document.querySelector(".popup_state_visible");
 }
 
-function closeByEsc(evt) {
+function closeByEsc (evt) {
 	const popup = findActivePopup(); 
 	if ((evt.key === "Escape") && (popup != null)) { 
-			togglePopup(popup);
-			document.removeEventListener('keydown',  closeByEsc)
+			togglePopup(popup);		
 	}
 }
 
-function setPopupEventListeners () {
-editbutton.addEventListener("click", () => togglePopup(popupProfile));
-profileCloseButton.addEventListener("click", () => togglePopup(popupProfile));
-addButton.addEventListener("click", () => togglePopup(popupCard));
-cardCloseButton.addEventListener("click", () => togglePopup(popupCard));
-imageContainerCloseBtn.addEventListener("click", () => togglePopup(popupImageContainer));
+function closeByClick (evt) {
+	const popup = findActivePopup();
+	console.log(evt.target);
+	if (evt.target.classList.contains('popup')) togglePopup(popup);
 }
+
+function setPopupEventListeners () {
+	editbutton.addEventListener("click", () => togglePopup(popupProfile));
+	profileCloseButton.addEventListener("click", () => togglePopup(popupProfile));
+	addButton.addEventListener("click", () => togglePopup(popupCard));
+	cardCloseButton.addEventListener("click", () => togglePopup(popupCard));
+	imageContainerCloseBtn.addEventListener("click", () => togglePopup(popupImageContainer));
+	closeAvatarForm.addEventListener("click", () => togglePopup(popupAvatarEdit));
+
+	}
 
 export {setPopupEventListeners, togglePopup, findActivePopup}
